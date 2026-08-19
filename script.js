f002c2a5ad706460a719f17e2666b41b8a518df5
Victor-Mavinga <victormavinga0@gmail.com>
2026-08-19 13:44:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function showMessage() {
     alert("Thank you for visiting my portfolio! You can contact me at victormavinga0@gmail.com");
 }
+function showMessage() {
+    alert("Thank you for visiting my portfolio!");
+}
+
+function toggleDarkMode() {
+
+    document.body.classList.toggle("dark-mode");
+
+    const button = document.getElementById("darkModeButton");
+
+    if (document.body.classList.contains("dark-mode")) {
+        button.innerHTML = "☀️ Light Mode";
+    } else {
+        button.innerHTML = "🌙 Dark Mode";
+    }
+}
